21c385f2d072b8fd9591da23b410f954e0c99753
kryeon <kry1105@ewha.ac.kr>
2026-07-07 14:57:54 +0900

    CommentApiController.java 만들기

---
+++ CommentApiController.java
@@ -0,0 +1,48 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class CommentApiController {
+
+    private final CommentService commentService;
+
+    public CommentApiController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    // 1. 댓글 조회
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
+        List<CommentDto> dtos = commentService.comments(articleId);
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+
+    // 2. 댓글 생성
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable Long articleId, 
+                                             @RequestBody CommentDto dto) {
+        CommentDto createdDto = commentService.create(articleId, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(createdDto);
+    }
+
+    // 3. 댓글 수정
+    @PatchMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id, 
+                                             @RequestBody CommentDto dto) {
+        CommentDto updatedDto = commentService.update(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
+    }
+
+    // 4. 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+        CommentDto deletedDto = commentService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(deletedDto);
+    }
+}
